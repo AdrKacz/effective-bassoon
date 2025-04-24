cf4775f47035ec5bc821aa39f475b420d45be5d4
AdrKacz <adrien.kaczmarek@gmail.com>
2025-04-24 12:29:17 +0100

    feat: remove Fb event as it doesn't work yet

--- web/assets/js/telemetry.mjs
+++ web/assets/js/telemetry.mjs
@@ -1,30 +1,53 @@
+const FB_ALLOWED = {
+    Lead: ['currency', 'value'],
+    Subscribe: ['currency', 'predicted_ltv', 'value'],
+};
+
 document.querySelectorAll('[data-event]').forEach(el => {
-  el.addEventListener('click', (e) => {
-    const eventName = el.getAttribute('data-event');
-    const href = el.getAttribute('href');
-    const isLink = el.tagName.toLowerCase() === 'a' && href;
+    el.addEventListener('click', (e) => {
+        const eventName = el.getAttribute('data-event');
+        const href = el.getAttribute('href');
+        const isLink = el.tagName.toLowerCase() === 'a' && href;
 
-    const data = {};
-    Array.from(el.attributes).forEach(attr => {
-      if (attr.name.startsWith('data-event-') && attr.name !== 'data-event') {
-        const key = attr.name.replace('data-event-', '');
-        data[key] = attr.value;
-      }
+        const data = {};
+        Array.from(el.attributes).forEach(attr => {
+          if (attr.name.startsWith('data-event-') && attr.name !== 'data-event') {
+            const key = attr.name.replace('data-event-', '');
+            data[key] = attr.value;
+          }
+        });
+
+        // Track event - Umami
+        if (typeof umami !== 'undefined' && typeof umami.track === 'function') {
+          umami.track(eventName, Object.keys(data).length ? data : undefined);
+        }
+        
+        // Track event - Facebook
+        // if (typeof fbq === 'function') {
+        //     const fbEvent = Object.keys(FB_ALLOWED).includes(eventName)
+        //         ? eventName
+        //         : Object.keys(FB_ALLOWED).includes(data['fb'])
+        //         ? data['fb']
+        //         : null;
+        //     if (fbEvent) {
+        //         const filteredData = Object.fromEntries(
+        //             Object.entries(data).filter(([key]) => FB_ALLOWED[fbEvent].includes(key))
+        //         );
+        //         console.log('Emit', fbEvent, filteredData);
+        //         fbq('track', fbEvent, filteredData)
+        //     }
+        // }
+
+
+
+        // If it's a link, prevent default and redirect after a short delay (unless it's a download)
+        if (isLink && !el.hasAttribute('download')) {
+            e.preventDefault();
+            // Small delay to allow umami to fire (you can tweak this value)
+            setTimeout(() => {
+                if (href === '#') window.location.reload();
+                else window.location.href = href;  
+            }, 150); // 150ms is usually enough
+        }
     });
-
-    // Trigger tracking
-    if (typeof umami !== 'undefined' && typeof umami.track === 'function') {
-      umami.track(eventName, Object.keys(data).length ? data : undefined);
-    }
-
-    // If it's a link, prevent default and redirect after a short delay (unless it's a download)
-    if (isLink && !el.hasAttribute('download')) {
-        e.preventDefault();
-        // Small delay to allow umami to fire (you can tweak this value)
-        setTimeout(() => {
-            if (href === '#') window.location.reload();
-            else window.location.href = href;  
-        }, 150); // 150ms is usually enough
-    }
-  });
 });
